938851c26bc71fbcc56d0a3f1da28a4215832750
Mitchell Lee <32659746+ZyrnDev@users.noreply.github.com>
2021-11-13 23:51:33 +1100

    Thank mr mitch dumb dumb head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from 'next/app'
 
-import { Box, ChakraProvider } from "@chakra-ui/react"
+import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react"
 
 import NavBar from '@/components/navbar'
 
+const theme = {
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+  },
+};
+
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <NavBar />
       <Box as="main" maxWidth="md" m="0 auto">
         <Component {...pageProps} />
