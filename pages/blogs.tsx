e0cef6428da104b96680925545535c5efee5e9be
Justin Chappell <justinac.1999@gmail.com>
2021-11-25 12:11:52 +1000

    Removed bad smells in blogs.tsx

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -14,7 +14,7 @@ export const getStaticProps = async () => {
   }
 }
 
-const Blogs: FC<InferGetStaticPropsType<typeof getStaticProps>>  = ({  posts, poop  }) => {
+const Blogs: FC<InferGetStaticPropsType<typeof getStaticProps>>  = ({  posts  }) => {
 
   return (
     <>
