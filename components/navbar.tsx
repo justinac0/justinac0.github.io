b93d483ad99046f1db354c9595573da08b21b36e
justinac0 <justinac.1999@gmail.com>
2022-06-14 12:00:55 +1000

    Added todo-flask to projects. Removed blogs

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,55 @@
 import {
-    Container,
-    Box,
-    Avatar,
-    HStack,
-    Heading,
-    Link
-} from "@chakra-ui/react"
+  Container,
+  Box,
+  Avatar,
+  HStack,
+  Heading,
+  Link,
+} from "@chakra-ui/react";
 
-import NextLink from "next/link"
+import NextLink from "next/link";
 
-const NavBar: React.FC = ({ children, ...props}) => {
-    return (
-        <Container p={2} maxW="container.md" position="sticky" top="0" w="100%" m="0 auto" bg={"gray.800"}{...props}>
-            <HStack spacing={4}>
-                <Box>
-                    <Avatar size="md" name="Justin Chappell" src="/spaceman.webp"></Avatar>
-                </Box>
-                <Box>
-                <Heading size="md" p={1} textTransform={'uppercase'}>Justin Chappell</Heading>
-                </Box>
-            </HStack>
-            <HStack spacing={4} mt={2}>
-                <NextLink href={'/'} passHref><Link color={"red.400"} fontWeight={"bold"}>ABOUT</Link></NextLink>
-                <NextLink href={'/projects'} passHref><Link color={"green.400"} fontWeight={"bold"}>PROJECTS</Link></NextLink>
-                <NextLink href={'/blogs'} passHref><Link color={"blue.400"} fontWeight={"bold"}>BLOGS</Link></NextLink>
-            </HStack>
-        </Container>
-    )
-}
+const NavBar: React.FC = ({ children, ...props }) => {
+  return (
+    <Container
+      p={2}
+      maxW="container.md"
+      position="sticky"
+      top="0"
+      w="100%"
+      m="0 auto"
+      bg={"gray.800"}
+      {...props}
+    >
+      <HStack spacing={4}>
+        <Box>
+          <Avatar
+            size="md"
+            name="Justin Chappell"
+            src="/spaceman.webp"
+          ></Avatar>
+        </Box>
+        <Box>
+          <Heading size="md" p={1} textTransform={"uppercase"}>
+            Justin Chappell
+          </Heading>
+        </Box>
+      </HStack>
+      <HStack spacing={4} mt={2}>
+        <NextLink href={"/"} passHref>
+          <Link color={"red.400"} fontWeight={"bold"}>
+            ABOUT
+          </Link>
+        </NextLink>
+        <NextLink href={"/projects"} passHref>
+          <Link color={"green.400"} fontWeight={"bold"}>
+            PROJECTS
+          </Link>
+        </NextLink>
+        {/* <NextLink href={'/blogs'} passHref><Link color={"blue.400"} fontWeight={"bold"}>BLOGS</Link></NextLink> */}
+      </HStack>
+    </Container>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,53 @@
-import '@/styles/prism-vsc-dark-plus.css';
+import "@/styles/prism-vsc-dark-plus.css";
 
-import type { AppProps } from 'next/app'
-import { Box, ChakraProvider, extendTheme, ColorModeScript } from "@chakra-ui/react"
-import NavBar from '@/components/navbar'
-import style from '@/styles/default'
-import Head from 'next/head';
+import type { AppProps } from "next/app";
+import {
+  Box,
+  ChakraProvider,
+  extendTheme,
+  ColorModeScript,
+} from "@chakra-ui/react";
+import NavBar from "@/components/navbar";
+import style from "@/styles/default";
+import Head from "next/head";
 
 const config = {
   initialColorMode: "dark",
-  useSystemColorMode: false,
-}
+  useSystemColorMode: true,
+};
 
 const styles = {
   global: {
     ".blog-content": {
       ...style,
-      "a": {
-        "color": "blue.400",
-        "text-decoration": "underline"
+      a: {
+        color: "blue.400",
+        "text-decoration": "underline",
       },
     },
   },
 };
 
-const theme = extendTheme({ config, styles })
+const theme = extendTheme({ config, styles });
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS={true}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}></ColorModeScript>
-      
+      <ColorModeScript
+        initialColorMode={theme.config.initialColorMode}
+      ></ColorModeScript>
+
       <Head>
         <title>justinac0 | Website</title>
-        <link rel="icon"  type="image/x-icon" href='/'></link>
+        <link rel="icon" type="image/x-icon" href="/"></link>
       </Head>
 
-      <NavBar/>
+      <NavBar />
       <Box as="main" maxWidth="md" m="0 auto" p={4}>
         <Component {...pageProps} />
       </Box>
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
