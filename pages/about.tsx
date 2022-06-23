56e99d17d56fef27d8c547fe0fa8cf1e1d1eabc3
justinac0 <justinac.1999@gmail.com>
2022-06-23 17:16:40 +1000

    fixed bad char

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,9 +2,9 @@ const About = () => {
   return (
     <>
       <p>
-        Hello! My name is Justin, I'm a Software Developer based in Australia. I
-        study Computer Science and Physics at QUT. I have been programming for a
-        little over 5 years now.
+        Hello! My name is Justin, I am a Software Developer based in Australia.
+        I study Computer Science and Physics at QUT. I have been programming for
+        a little over 5 years now.
       </p>
       <br />
     </>
