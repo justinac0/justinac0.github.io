3f36c00d088abb3a6bee3c4d5e2837756330cc96
justinac <52983526+justinac0@users.noreply.github.com>
2022-06-23 17:17:52 +1000

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <>
       <p>
-        Hello! My name is Justin, I'm a Software Developer based in Australia. I
+        Hello! My name is Justin, I am a Software Developer based in Australia. I
         study Computer Science and Physics at QUT. I have been programming for a
         little over 5 years now.
       </p>
