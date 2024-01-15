1c86bcba8b2711ae0f5658d060602ed50b5b81d2
justin <justinac.1999@gmail.com>
2024-01-15 11:27:57 +1000

    Updated About secion.

--- app/routes/about/route.tsx
+++ app/routes/about/route.tsx
@@ -6,15 +6,25 @@ export default function About() {
             <>
                 <h1 className="text-4xl text-center">About</h1>
                 <br />
-
-                <p>Hello! my name is Justin and I am currently an undergraduate student at QUT, undertaking a double bachelor of Science and IT {"("}majoring in physics and computer science respectively{")"}.</p>
+                <p>
+                    Hello, my name is Justin and I{'\’'}m an undergraduate student at <a  className="text-blue-400 underline" href="https://www.qut.edu.au/">QUT</a>.
+                </p>
                 <br />
-                <p>Always eagar to improve I spend my spare time creating software not always directly related to course content covered. Some of the projects I have worked on over the years
-                    covering topics such as graphics programming, game development, mobile application, website design and development, simuluation phyiscs, IoT and the list goes on.</p>
+                <p>
+                    I{'\’'}m studying a double bachelor of Science and Information Technology (majoring in Physics and Computer Science).
+                </p>
                 <br />
-                <p>This website serves as a hub where I will archive interesting projects I have made, write about things I have been learning (mainly for me to come back to) and be a place where future employers can easily view my work and contact me.</p>
+                <p>
+                    Always eagar to learn and improve my skills, I like to spend my spare time creating software not always directly related to course content covered. For example, some projects and topics I have delved into are graphics programming, game development, mobile applications, website design and development, simuluation phyiscs, IoT and the list goes on.
+                </p>
                 <br />
-                <p>If you wish to contact me, feel free to through the contact page.</p>
+                <p>
+                    This website serves as a hub where I will archive interesting projects that I have worked on, write about things I have been learning and be a place where future employers can easily view my work and contact me.
+                </p>
+                <br />
+                <p>
+                    If you wish to contact me, feel free to through the contact page.
+                </p>
             </>
         } />
 
