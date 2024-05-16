4d6df89c512280f0d49f7b7260bec8ec1c32de00
justin <justinac.1999@gmail.com>
2024-05-16 20:59:15 +1000

    modified:   app/components/Copyright.tsx 	modified:   app/components/NavBar.tsx 	modified:   app/routes/_index.tsx

--- app/components/Copyright.tsx
+++ app/components/Copyright.tsx
@@ -1,5 +1,5 @@
 export default function Copyright() {
     return (
-        <p className="fixed w-screen text-center bottom-0 text-white/50 bg-black/30 p-1">Copyright © 2023-2024 Justin Chappell</p>
+        <p className="fixed w-screen text-center bottom-0 text-white/50 bg-black/30 p-1 text-xl">Copyright © 2023-2024 Justin Chappell</p>
     );
 }
