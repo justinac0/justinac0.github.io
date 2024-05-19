8ea0abf8b64320ccbc67cc4416917a6b11d0cd56
justin <justinac.1999@gmail.com>
2024-05-19 12:03:47 +1000

    modified:   app/components/ImageViewer.tsx 	modified:   app/components/PortfolioItem.tsx 	modified:   app/styles/portfolio.css 	modified:   public/portfolio/website.png

--- app/components/ImageViewer.tsx
+++ app/components/ImageViewer.tsx
@@ -1,5 +1,31 @@
-export default function ImageViewer() {
+import { useState } from "react";
+
+interface ImageViewerParams {
+    img: string
+};
+
+export default function ImageViewer({
+    img
+}: ImageViewerParams) {
+    const [isLarge, setIsLarge] = useState(false);
+
+    const view_image = () => {
+        setIsLarge(!isLarge);
+
+        if (isLarge) {
+
+        }
+    }
+
     return (
-        <></>
+        <>
+            <div>
+                <img className="portfolio-item-image" src={img} alt="" onClick={view_image}/>
+                {isLarge &&
+                <div className="enlarge-backdrop" onClick={view_image}>
+                    <img className="enlarge-image" src={img} alt="" onClick={view_image}/>
+                </div>}
+            </div>
+        </>
     );
 }
